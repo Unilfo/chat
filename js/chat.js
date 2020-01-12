d2f978e0a26ea163f03b554ee6286357bba20137
Unilfo <kokopal13@gmail.com>
2020-01-12 18:05:39 +0300

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -208,7 +208,7 @@ function PopulateFriendList() {
                       <div class="clo-md-2">
                           <img src="${user.photoURL}" class="rounded-circle friend-logo">
                       </div>
-                      <div class="col-md-10" style="cursor:pointer;">
+                      <div class="col-md-10 col-8" style="cursor:pointer;">
                           <div class="name">${user.name}</div>
                       </div>
                   </div>
