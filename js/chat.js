285e657691e203df016ec65fec8f5cbffb6648f1
Unilfo <kokopal13@gmail.com>
2020-01-14 23:42:22 +0300

    Add files via upload

--- js/chat.js
+++ js/chat.js
@@ -97,6 +97,10 @@ function LoadChatMessages(chatKey, friendPhoto) {
 }
 
 function hideChatList() {
+    document.getElementById("side-1").classList.add("d-none", "d-md-block");
+    document.getElementById("side-2").classList.remove("d-none");
+}
+function showChatList() {
     document.getElementById("side-1").classList.remove("d-none", "d-md-block");
     document.getElementById("side-2").classList.add("d-none");
 }
